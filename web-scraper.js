6a80741dbf3148838e42e1f4fd881f2bc43d2f24
Peter Abolude <aboludepeter@gmail.com>
2023-07-01 11:07:52 +0100

    demo git pages

--- web-scraper.js
+++ web-scraper.js
@@ -1,24 +1,32 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+import axios from "https://cdn.skypack.dev/axios";
+import cheerio from "https://cdn.skypack.dev/cheerio";
 
-const url = "https://www.gsmarena.com/apple-phones-48.php";
+function showPhones() {
+  const url = "https://www.gsmarena.com/apple-phones-48.php";
 
-axios.get(url)
-    .then(res =>{
-        const html = res.data;
-        const $ = cheerio.load(html);
+  axios
+    .get(url)
+    .then((res) => {
+      const html = res.data;
+      const $ = cheerio.load(html);
 
-        const phones = {};
+      const phones = {};
 
-        $(".makers li").each((index, element) => {
-          const name = $(element).find("span").text();
+      $(".makers li").each((index, element) => {
+        const name = $(element).find("span").text();
 
-          phones[`phone${index + 1}`] = name;
-        });
+        phones[`phone${index + 1}`] = name;
+      });
 
-        const phonesJson = JSON.stringify(phones, null, 1);
-        console.log((phonesJson));
+      const phonesListJson = JSON.stringify(phones, null, 1);
+
+      const phoneListDiv = document.getElementById("phoneList");
+      phoneListDiv.innerHTML(phonesListJson);
     })
-    .catch(error =>{
-        console.log('Error loading webpage:', error);
-    })
+    .catch((error) => {
+      console.log("Error loading webpage:", error);
+    });
+}
+
+const phoneButton = document.getElementById("phoneListButton");
+phoneButton.addEventListener("click", showPhones);
